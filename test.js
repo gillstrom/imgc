c4027e671a591fa0e1cd8575ccfef1b52f4a4ecd
Andreas Gillström <andreasgillstrom@gmail.com>
2015-10-07 17:41:13 +0200

    ES2015ify tests

--- test.js
+++ test.js
@@ -3,37 +3,37 @@ var stat = require('fs').stat;
 var test = require('ava');
 var imgc = require('./');
 
-test('Should output jpeg', function (t) {
+test('Should output jpeg', t => {
 	t.plan(3);
 
-	imgc('./fixtures/*.CR2 ./fixtures/*.png', './fixtures/output', {format: 'jpeg', quality: 'medium'}, function (err) {
+	imgc('./fixtures/*.CR2 ./fixtures/*.png', './fixtures/output', {format: 'jpeg', quality: 'medium'}, err => {
 		t.assert(!err, err);
 
-		stat('./fixtures/output/test_CR2.jpg', function (err) {
+		stat('./fixtures/output/test_CR2.jpg', err => {
 			t.assert(!err, err);
 		});
 
-		stat('./fixtures/output/test_PNG.jpg', function (err) {
+		stat('./fixtures/output/test_PNG.jpg', err => {
 			t.assert(!err, err);
 		});
 	});
 });
 
-test('Should output tiff', function (t) {
+test('Should output tiff', t => {
 	t.plan(4);
 
-	imgc('./fixtures/*.*', './fixtures/output', {format: 'tiff'}, function (err) {
+	imgc('./fixtures/*.*', './fixtures/output', {format: 'tiff'}, err => {
 		t.assert(!err, err);
 
-		stat('./fixtures/output/test_CR2.tiff', function (err) {
+		stat('./fixtures/output/test_CR2.tiff', err => {
 			t.assert(!err, err);
 		});
 
-		stat('./fixtures/output/test_PNG.tiff', function (err) {
+		stat('./fixtures/output/test_PNG.tiff', err => {
 			t.assert(!err, err);
 		});
 
-		stat('./fixtures/output/test_JPG.tiff', function (err) {
+		stat('./fixtures/output/test_JPG.tiff', err => {
 			t.assert(!err, err);
 		});
 	});
